2e85a0359436a711489c99b8bb7acf2d2a9adab6
刘白菜 <liushuai.baicai@hotmail.com>
2020-05-06 16:27:24 +0800

    fix path

--- room-map-receiver/puma.rb
+++ room-map-receiver/puma.rb
@@ -1,12 +1,9 @@
 #!/usr/bin/env puma
 # frozen_string_literal: true
 
-# workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
-preload_app!
-
-rackup      DefaultRackup
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
