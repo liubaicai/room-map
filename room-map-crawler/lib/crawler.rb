b9e1b1996aba899c1e5c7cfeaf317b4d873b6a55
刘白菜 <liushuai.baicai@hotmail.com>
2021-04-22 11:53:39 +0800

    update

--- room-map-crawler/lib/crawler.rb
+++ room-map-crawler/lib/crawler.rb
@@ -14,18 +14,18 @@ Dir[File.dirname(__FILE__) + '/*/*.rb'].sort.each { |file| require file }
 Kimurai.configure do |config|
   # Default logger has colored mode in development.
   # If you would like to disable it, set `colorize_logger` to false.
-  # config.colorize_logger = false
+  config.colorize_logger = true
 
   # Logger level for default logger:
   # config.log_level = :info
-  config.log_level = :info
+  config.log_level = :warn
 
   # Custom logger:
   # config.logger = Logger.new(STDOUT)
 
   # Custom time zone (for logs):
-  # config.time_zone = "UTC"
-  # config.time_zone = "Europe/Moscow"
+  config.time_zone = "UTC"
+  config.time_zone = "Asia/China"
 
   # Provide custom chrome binary path (default is any available chrome/chromium in the PATH):
   # config.selenium_chrome_path = "/usr/bin/chromium-browser"
